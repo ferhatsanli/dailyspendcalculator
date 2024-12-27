75be94dbc693c1b619bc78dff1b4cfea322e0a05
Ferhat Sanli <ferhatsanli.nl@gmail.com>
2024-12-27 01:23:29 +0100

    saves date, days left textview

--- app/src/main/java/com/ferhat/dailyspendcalculator/MainActivity.kt
+++ app/src/main/java/com/ferhat/dailyspendcalculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ferhat.dailyspendcalculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -8,15 +9,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ferhat.dailyspendcalculator.databinding.ActivityMainBinding
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG: String = "MainAct"
+    private val TAG: String = "MainFunc"
     private lateinit var funcsData: FunctionsData
     private lateinit var funcsCalculator: FunctionsCalculate
     private var currentSalary: Int = 0
+    private var currentDate: String = ""
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -27,35 +31,51 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.i(TAG, "onCreate: classes initializing")
+        // Function Class init
         funcsData = FunctionsData(this)
         funcsCalculator = FunctionsCalculate(this)
 
+        // Read money from previous session
+        Log.i(TAG, "onCreate: reading salary")
         currentSalary = funcsData.getCurrentSalary()
         Log.i(TAG, "onCreate: salary=$currentSalary")
         binding.txtSalary.setText("$currentSalary")
 
+        // Read date from previous session
+        currentDate = funcsData.getDate()
+        Log.i(TAG, "onCreate: date from db is '$currentDate'")
+        binding.txtDate.setText(currentDate)
 
         binding.btnSalarySave.setOnClickListener() {
             saveSalary()
+            saveDate()
         }
 
-        binding.btnCalculate.setOnClickListener(){
+        binding.btnCalculate.setOnClickListener() {
             val money: Int = Integer.parseInt(binding.txtSalary.text.toString())
             val targetDate: String = binding.txtDate.text.toString()
             val day = Integer.parseInt(targetDate.split('/')[0])
             val month = Integer.parseInt(targetDate.split('/')[1])
             val year = Integer.parseInt(targetDate.split('/')[2])
-            val totalDays: Long = funcsCalculator.daysBetween(day,  month, year)
+            val totalDays: Long = funcsCalculator.daysBetween(day, month, year)
             val dailySpend: Int = funcsCalculator.dailySpend(money, totalDays)
-            binding.txtResult.text = "$dailySpend"
+            binding.txtResult.text = "$dailySpend Euro"
+            binding.txtDaysLeft.text = "$totalDays days left"
             saveSalary()
+            saveDate()
         }
     }
 
-    fun saveSalary(){
+    fun saveSalary() {
         currentSalary = Integer.parseInt(binding.txtSalary.text.toString())
         funcsData.saveSalary(currentSalary)
-        Log.i(TAG,"salary saved to db")
+        Log.i(TAG, "salary saved to db")
+    }
+
+    fun saveDate() {
+        Log.i(TAG, "saveDate: saving date")
+        funcsData.saveDate(binding.txtDate.text.toString())
     }
 }
 
--- app/src/main/java/com/ferhat/dailyspendcalculator/funcsData.kt
+++ app/src/main/java/com/ferhat/dailyspendcalculator/funcsData.kt
@@ -1,32 +1,69 @@
 package com.ferhat.dailyspendcalculator
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.util.Log
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity.MODE_PRIVATE
+import java.time.LocalDate
 
 class FunctionsData(context: Context) {
     val TAG = "DataFunc"
-    private val dataName = "SalaryData"
-    private val salaryName = "CurrentSalary"
-    private val sharedPref = context.getSharedPreferences(dataName, Context.MODE_PRIVATE)
+    private val dbName = "SalaryData"
+    private val keySalary = "CurrentSalary"
+    private val keyDate = "date"
+    private val sharedPref = context.getSharedPreferences(dbName, Context.MODE_PRIVATE)
 
-    fun saveSalary(salary: Int){
+    private fun saveData(key: String, value: Any) {
         val editor = sharedPref.edit()
-        
-        editor.putInt(salaryName, salary)
+
+        when (value) {
+            is Int -> editor.putInt(key, value)
+            is String -> editor.putString(key, value)
+        }
         editor.apply()
-        Log.i(TAG, "saveSalary: new salary saved ($salary)")
+        Log.i(TAG, "saveData: $key saved, value is $value")
     }
 
-    fun getCurrentSalary(): Int{
-        if (sharedPref.contains(salaryName)) {
-            val currsalary = sharedPref.getInt(salaryName, 0)
-            Log.i(TAG, "getCurrentSalary: $salaryName found ($currsalary)")
-            return currsalary
+//    private fun getData(key: String): Any? {
+//        if (sharedPref.contains(key)) {
+//            Log.i(TAG, "getData: key ($key) found")
+//            when (sharedPref.getString("${key}_type", null)) {
+//                "int" -> {
+//                    Log.i(TAG, "getData: key=$key")
+//                    return sharedPref.getInt(key, 0)
+//                }
+//
+//                "string" -> return sharedPref.getString(key, "")
+//                else -> {
+//                    Log.i(TAG, "getData: 404: type=${key}_type")
+//                    return null
+//                }
+//            }
+//        }
+//        return null
+//    }
+
+    fun saveSalary(salary: Int) {
+        saveData(keySalary, salary)
+    }
+
+    fun saveDate(date: String) {
+        saveData(keyDate, date)
+    }
+
+    fun getDate(): String {
+        var result = sharedPref.getString(keyDate, "")
+        if (result == null || result == "") {
+            val plus30days = LocalDate.now().plusDays(30)
+            result = "${plus30days.dayOfMonth}/${plus30days.monthValue}/${plus30days.year}"
         }
-        Log.i(TAG, "getCurrentSalary: not found in db")
-        return 0
+        return result
+    }
+
+    fun getCurrentSalary(): Int {
+        Log.i(TAG, "getCurrentSalary: started")
+//        return getData(keySalary) as Int
+
+        val currsalary = sharedPref.getInt(keySalary, 0)
+        Log.i(TAG, "getCurrentSalary: $keySalary found ($currsalary)")
+        return currsalary
     }
 }
